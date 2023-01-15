764318ff9d21ddaa729b8d9611ef74cdb7e2ceae
albim72 <marcin.albiniak72@gmail.com>
2023-01-15 19:45:46 +0100

    Create dane.php

---
+++ DATAFLOW/dane.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>odpowiedź</title>
+</head>
+<body>
+    <h3>Dane z formularza...</h3>
+    Witaj <?php echo $_POST["imie"];?><br>
+    Adres e-mail: <?php echo $_POST["email"];?><br>
+</body>
+</html>
