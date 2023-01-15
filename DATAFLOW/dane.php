96914c3fda0fb163e7dd0d8df2816bf5d6f81ade
albim72 <marcin.albiniak72@gmail.com>
2023-01-15 20:39:36 +0100

    Update dane.php

--- DATAFLOW/dane.php
+++ DATAFLOW/dane.php
@@ -8,7 +8,34 @@
 </head>
 <body>
     <h3>Dane z formularza...</h3>
-    Witaj <?php echo $_POST["imie"];?><br>
-    Adres e-mail: <?php echo $_POST["email"];?><br>
+    <?php    
+    $name=$_POST['imie'];
+    $email=$_POST['email'];
+    ?>
+    Witaj <?php echo "$name"?><br>
+    Adres e-mail: <?php echo "$email"?><br>
+
+    <?php
+    $servername = "localhost:3306";
+    $username = "root";
+    $password = "abc123";
+    $dbname = "formularze_php";
+
+    $conn = new mysqli($servername,$username,$password,$dbname);
+    if($conn->connect_error){
+        die("Połączenie niemożliwe: ".$conn -> connect_error);
+    }
+
+    $sql = "INSERT INTO rejestracja(imie,email) values ('$name','$email')";
+
+
+    if(mysqli_query($conn,$sql)){
+        echo "rekord dodano!";
+    }else{
+        echo "Error: ".$sql."-".mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
+    ?>
 </body>
 </html>
