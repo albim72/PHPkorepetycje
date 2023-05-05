00026329e0e27dece79444f9d367f631b26f2ae5
albim72 <marcin.albiniak72@gmail.com>
2023-05-05 19:22:11 +0200

    Update connect.php

--- 0505/connect.php
+++ 0505/connect.php
@@ -1,1 +1,21 @@
+<?php
+$username = "root";
+$passwd = "abc123";
+$servername = "localhost:3306";
 
+$conn = new mysqli($servername,$username,$passwd);
+
+if($conn->connect_error){ 
+    die("Połączenie niemożliwe... ".$conn->connect_error);
+}
+
+$sql = "CREATE DATABASE IF NOT EXISTS klienci";
+if($conn->query($sql) === TRUE){
+    echo "baza została utworzona!";
+}
+else {
+    echo "błąd echo".$conn->error;
+}
+$conn->close();
+
+?>
