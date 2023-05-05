ba6a5f6ea9b8c7900a9c2718f71f614209c1652c
albim72 <marcin.albiniak72@gmail.com>
2023-05-05 20:15:32 +0200

    Add files via upload

---
+++ 0505/form_dane/dane.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dane z formy</title>
+</head>
+<body>
+    Witaj <?php echo $_POST["imie"];?><br>
+    Adres e-mail <?php echo $_POST["email"];?><br>
+
+</body>
+</html>
