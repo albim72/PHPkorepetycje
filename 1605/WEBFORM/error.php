f2e5bb017e12fada3aca7fcbba57d192b35c5ba9
albim72 <marcin.albiniak72@gmail.com>
2023-05-24 20:27:24 +0200

    Create error.php

---
+++ 1605/WEBFORM/error.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Untitled Document</title>
+    <!-- Bootstrap -->
+	<link href="css/bootstrap-4.4.1.css" rel="stylesheet">
+    <style>
+        .wrapper {
+            width:750px;
+            margin:0 auto;
+        }
+    </style>
+	
+  </head>
+  <body>
+  	<!-- body code goes here -->
+
+        <div class="wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="page-header">
+                            <h1>Invalid Request</h1>
+
+                        </div>
+                        <div class="alert alert-danger fade in">
+                            <p>Sorry, you've made an invalid request.
+                                Please <a href="index.php" class="alert-link">go back</a>
+                                and try again.</p>
+                            </p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+	<script src="js/jquery-3.4.1.min.js"></script>
+
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/popper.min.js"></script> 
+	<script src="js/bootstrap-4.4.1.js"></script>
+  </body>
+</html>
